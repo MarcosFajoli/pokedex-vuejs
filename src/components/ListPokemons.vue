92e5aa9678adaf9970258c3e8d9f88b1780398cb
Marcos Fajoli <markosalmeidaa@gmail.com>
2023-08-29 22:27:24 -0300

    mapping home page

---
+++ src/components/ListPokemons.vue
@@ -0,0 +1,20 @@
+<script setup>
+const pokemon = defineProps(["name", "url"])
+
+</script>
+
+<template>
+    <div class="col-4 text-capitalize">
+        <div class="card mb-1">
+            <img class="card-img-top" src="pokebola.png" :alt="pokemon.name">
+            <div class="card-body">
+                <h5 class="card-title">{{ pokemon.name }}</h5>
+                <a :href="pokemon.url" class="btn btn-primary">Ver Pokémon</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup>
 
-import { onMounted, reactive, ref } from 'vue';
+import { onMounted, reactive, ref } from 'vue'
+import ListPokemons from "../components/ListPokemons.vue"
+
+let pokemons = reactive(ref())
 
 onMounted(() => {
-  fetch("https://pokeapi.co/api/v2/ability/?limit=20&offset=0")
+  fetch("https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0")
   .then(response => response.json())
   .then(response => {
+    pokemons.value = response.results
     console.log(response)
   })
 })
@@ -25,8 +29,17 @@ onMounted(() => {
             </div>
           </div>
         </div>
-        <div class="col-sm-12 col-md-6">
-          ADS 2023
+        <div class="col-sm-12 col-md-6 mb-5">
+          <div class="card">
+            <div class="card-body row">
+              <ListPokemons 
+              v-for="pokemon in pokemons"
+              :key="pokemon.name"
+              :name="pokemon.name"
+              :url="pokemon.url"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
