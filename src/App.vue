87775e986a28b08f9992ff9baded519544fbe21b
Marcos Fajoli <markosalmeidaa@gmail.com>
2023-09-20 14:12:38 -0300

    remove nav and footer

--- src/App.vue
+++ src/App.vue
@@ -5,44 +5,9 @@ let num = ref(0);
 </script>
 
 <template>
-  <nav
-    class="navbar navbar-expand-lg navbar-dark bg-primary border-bottom border-body"
-  >
-    <div class="container-fluid">
-      <routerLink to="/" class="navbar-brand">Pokédex</routerLink>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <routerLink to="/" class="nav-link" aria-current="page"
-              >Home</routerLink
-            >
-          </li>
-          <li class="nav-item">
-            <routerLink to="/about" class="nav-link">About</routerLink>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
   <div class="main">
     <router-view></router-view>
   </div>
-
-  <footer class="bg-primary text-light">
-    <div>Unimar | ADS | {{ new Date().getFullYear() }}</div>
-  </footer>
 </template>
 
 <style>
