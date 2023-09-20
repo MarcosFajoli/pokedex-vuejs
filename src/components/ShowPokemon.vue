ead12737232e70a0e2b0e9b59d0c13cde6f1552c
Marcos Fajoli <markosalmeidaa@gmail.com>
2023-09-20 09:56:25 -0300

    add color pick from api

--- src/components/ShowPokemon.vue
+++ src/components/ShowPokemon.vue
@@ -1,19 +1,19 @@
 <script setup>
+import typeColor from "../utils/typeColor";
+import { onMounted } from "vue";
+
 const pokemon = defineProps({
-  value: {
-    id: Number,
-    name: String,
-    sprites: {
-      other: {
-        dream_world: {
-          front_default: String,
-        },
-      },
-    },
-  },
+  value: Object,
 });
 
 const pokemonSrc = pokemon.value.sprites.other.dream_world.front_default;
+
+onMounted(() => {
+  document.getElementById("card-text").style.color =
+    typeColor[pokemon.value.types[0].type.name];
+});
+
+console.log(pokemon.value.types[0].type.name);
 </script>
 
 <template>
@@ -22,11 +22,11 @@ const pokemonSrc = pokemon.value.sprites.other.dream_world.front_default;
       class="card-img-top"
       :src="pokemonSrc"
       alt="Card image cap"
-      style="max-height: 65vh"
+      style="max-height: 30vh"
     />
     <div class="card-body">
       <h5 class="card-title text-capitalize">{{ pokemon.value.name }}</h5>
-      <p class="card-text">
+      <p class="card-text" id="card-text">
         Some quick example text to build on the card title and make up the bulk
         of the card's content.
       </p>
